14e7f41deabfcd9fcc1a241323cc3b0292d7945d
Gustavo Luvizotto Cesar <gustavoluvizotto@gmail.com>
2017-06-05 15:25:14 +0200

    Ball movement with script.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();		
+	}
+	
+    // giving the flow movement. Fixing the update iteractions
+    private void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement);
+    }
+}
